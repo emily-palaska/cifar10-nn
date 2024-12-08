4fb50470f2330c129a527accbfab0463b3f9bd0e
Emily Palaska <aimilia.p2@gmail.com>
2024-12-08 19:11:00 +0200

    more compact dataset loading

--- source/svm_main.py
+++ source/svm_main.py
@@ -4,13 +4,11 @@ from baselines.cifar10 import Cifar10
 def main():
     # Load the CIFAR-10 dataset
     dataset = Cifar10(normalization='min-max')
-    x_train, y_train, x_test, y_test = dataset.get_train_test()
+    x_train, y_train, x_test, y_test = dataset.get_train_test_split()
 
     svm_classifier = SVMClassifier(learning_rate=0.001, lambda_param=0.01, n_iters=1000)
     svm_classifier.fit(x_train, y_train)
     svm_classifier.evaluate(x_test, y_test)
-
-
 
 if __name__ == '__main__':
     main()
