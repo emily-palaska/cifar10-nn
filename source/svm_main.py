2f5123759dae489de51939023b2104cfbcd44d57
Emily Palaska <aimilia.p2@gmail.com>
2024-12-14 23:50:27 +0200

    module update for imports

--- source/svm_main.py
+++ source/svm_main.py
@@ -1,14 +1,17 @@
-from svm.scratch.svm_classifier import SVMClassifier
 from baselines.cifar10 import Cifar10
+import svm.scratch as mysvm
 
 def main():
     # Load the CIFAR-10 dataset
     dataset = Cifar10(normalization='min-max')
     x_train, y_train, x_test, y_test = dataset.get_train_test_split()
 
-    # add one vs all logic or one vs one logic
+    # One vs all logic
+    target = 1
+    y_train, y_test = mysvm.one_vs_all(y_train, target), mysvm.one_vs_all(y_test, target)
 
-    svm_classifier = SVMClassifier(learning_rate=0.001, lambda_param=0.01, n_iters=1000)
+    # Initialize classifier
+    svm_classifier = mysvm.SVMClassifier(learning_rate=0.0001, lambda_param=0.01, n_iters=100)
     svm_classifier.fit(x_train, y_train)
     svm_classifier.evaluate(x_test, y_test)
 
